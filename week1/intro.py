4757b8db6532fc4ed2ceb0a5ce793e362477d5f1
alvin <alvinsunyixiao@gmail.com>
2016-09-29 20:05:36 +0800

    update intro

--- week1/intro.py
+++
@@ -1,6 +0,0 @@
-'''
-Hello All!
-We will be using PYTHON for this Modeling Class. If you have any language preference or previous coding experience, feel free to go with whatever computer language you are comfortable with.
-However, I cannot promise to provide full support for all languages. You will have to read all sorts of documentations on your own in order to accomplish different tasks.
-I wrote this introduction page in English because the coding references that you might have to read are mostly written in English. You should adapt yourself to this foreign language environment starting from now on.
-'''
