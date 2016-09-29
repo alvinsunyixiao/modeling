be6fe32a048f08c4b7f826ad15990e4399bae68a
alvin <alvinsunyixiao@gmail.com>
2016-09-29 21:23:32 +0800

    finish direction and playground

---
+++ week1/playground.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+If you are using a mac, this is an empty python file to mess around with.
+Instructions:
+    1. Download this file from my github repo
+    2. Edit this file with whatever text editor you feel like using including PyCharm, Xcode, Atom, Sublime Text, Vim etc.
+    3. Open up a terminal window
+    4. Simply drag this file to the terminal and hit Enter to run it.
+
+Sorry for the Windows user ;)
+'''
+
+#Edit your code below
+print("Hello World")
